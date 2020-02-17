77c31861fd19b4eb80484b573abdcccd54fdc36f
Alfredo Savi <alfredosavi@hotmail.com>
2020-02-17 15:18:43 -0300

    First Commit

---
+++ data.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: "Alan Walker - Spectre [NCS Release]",
+    artist: "Alan Walker",
+    cover: "ncs1.jpg",
+    file: "music1.mp3"
+  },
+  {
+    title: "Disfigure - Blank [NCS Release]",
+    artist: "Disfigure",
+    cover: "ncs2.jpg",
+    file: "music2.mp3"
+  }
+];
